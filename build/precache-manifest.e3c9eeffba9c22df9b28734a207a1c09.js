02928b38b30bce6a764ec09b4acc83b4bafa3295
raeesvds <raees.vds@gmail.com>
2022-09-07 21:23:15 +0200

    added ring sizer

---
+++ build/precache-manifest.e3c9eeffba9c22df9b28734a207a1c09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5fc5f9714d38ed3ae9115505033434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce98e14fa8c55dd4a6ba",
+    "url": "/static/css/main.e23ca816.chunk.css"
+  },
+  {
+    "revision": "0254d684c1cd44511cdb",
+    "url": "/static/js/2.e4a780c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e4a780c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce98e14fa8c55dd4a6ba",
+    "url": "/static/js/main.bec6aec8.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "8ae7af5b479ee9ece05111ca7f26a87f",
+    "url": "/static/media/book-summary-thumbnail.8ae7af5b.JPG"
+  },
+  {
+    "revision": "78beb134ea9ac433be3e32c2e6a72b91",
+    "url": "/static/media/programming-concept_82472-703.78beb134.webp"
+  },
+  {
+    "revision": "549ce4af9671ba007f7a8f1933872033",
+    "url": "/static/media/ring-sizer.549ce4af.JPG"
+  },
+  {
+    "revision": "a69e3c609bf3e10de365129cf56057ae",
+    "url": "/static/media/rugby-thumbnail.a69e3c60.JPG"
+  }
+]);
